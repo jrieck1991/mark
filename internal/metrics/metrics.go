fb9346666ad39a1fbc9b8eaeb5d5aabf1d347456
jrieck <jack.rieck@sendgrid.com>
2020-04-08 21:06:51 -0600

    expose metrics (#3)
    * expose metrics
    
    * make serve private

--- internal/metrics/metrics.go
+++ internal/metrics/metrics.go
@@ -1,1 +1,27 @@
 package metrics
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+func init() {
+	go func() {
+		if err := serve(":9000"); err != nil {
+			fmt.Println(err)
+		}
+	}()
+}
+
+func serve(addr string) error {
+
+	http.Handle("/metrics", promhttp.Handler())
+
+	if err := http.ListenAndServe(addr, nil); err != nil {
+		return err
+	}
+
+	return nil
+}
