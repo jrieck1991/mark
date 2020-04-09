ae8713a22b6bd460b1b1668efa9fc34028e491ee
jrieck <jack.rieck@sendgrid.com>
2020-04-08 22:07:43 -0600

    counters (#5)
    * counters
    
    * builds now
    
    * metrics exposed and detected for server
    
    * recv
    
    * working server dashboard and saved

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"net"
 
-	_ "github.com/jrieck1991/mark/internal/metrics"
+	"github.com/jrieck1991/mark/internal/metrics"
 	"github.com/jrieck1991/mark/internal/pipe"
 
 	"google.golang.org/grpc"
@@ -13,6 +13,14 @@ const addr = ":7777"
 
 func main() {
 
+	// serve metrics via http
+	go func() {
+		if err := metrics.Serve(":9000"); err != nil {
+			panic(err)
+		}
+	}()
+
+	// listen on socket
 	l, err := net.Listen("tcp", addr)
 	if err != nil {
 		panic(err)
@@ -21,6 +29,7 @@ func main() {
 	p := pipe.Server{}
 	s := grpc.NewServer()
 
+	// serve requests
 	pipe.RegisterPipeServer(s, &p)
 	if err := s.Serve(l); err != nil {
 		panic(err)
--- internal/pipe/pipe.go
+++ internal/pipe/pipe.go
@@ -1,23 +1,50 @@
 package pipe
 
 import (
-	"fmt"
 	"io"
+
+	"github.com/jrieck1991/mark/internal/metrics"
 )
 
 type Server struct{}
 
+const (
+	// base
+	namespace string = "app"
+	subsystem string = "server"
+
+	// metrics
+	msgRecv    string = "message_recv"
+	bytesRecv  string = "bytes_recv"
+	recvErr    string = "recv_error"
+	recvErrEOF string = "recv_error_eof"
+)
+
+// Ingest data
 func (s *Server) Ingest(srv Pipe_IngestServer) error {
 
+	// init metric counters
+	counters := metrics.Counters(namespace, subsystem, []string{
+		msgRecv, bytesRecv, recvErr, recvErrEOF,
+	})
+
+	// receive data
 	for {
+
 		data, err := srv.Recv()
 		if err != nil {
 			if err == io.EOF {
-				return nil
+				counters[recvErrEOF].Inc()
+				continue
 			}
-			return err
+			counters[recvErr].Inc()
+			continue
 		}
 
-		fmt.Println("data received:", data.GetData())
+		// record message sent
+		counters[msgRecv].Inc()
+
+		// record bytes
+		counters[bytesRecv].Add(float64(len(data.GetData())))
 	}
 }
